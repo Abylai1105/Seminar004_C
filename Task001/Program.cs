77acd3d04703585845a4924204b8ef0d1f1ffe7f
Abylai <aba.omar@mail.ru>
2022-05-02 14:33:26 +0600

    add Task001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for(int i = 1; i <= number; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine("Otvet: " + sum);
