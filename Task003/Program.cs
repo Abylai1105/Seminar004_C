d27e5d39bf5119047fff75a11d24ac2eeb036fae
Abylai <aba.omar@mail.ru>
2022-05-02 18:33:45 +0600

    add Task004

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,11 +1,33 @@
 ﻿Console.Write("Введите число ");
 int fact = Convert.ToInt32(Console.ReadLine());
-
+// Первое решение
+/*
 int sum = 1;
 
 for(int i = 1; i <= fact; i++)
 {
     sum *= i;
 }
+Console.WriteLine("Otvet: " + sum);*/
 
-Console.WriteLine("Otvet: " + sum);
+// Второе решение
+
+int result =1;
+int index = 1;
+while (index <= fact)
+{
+    result = result * index;
+    index++;
+}
+Console.WriteLine(result);
+
+// Третье решение
+/*
+int result =1;
+int index = 1;
+do
+{
+    result = result * index;
+    index++;
+}  while (index <= fact);
+Console.WriteLine(result); */
