81561ea842899e9945b93a1756553d665cb0a9df
Abylai <aba.omar@mail.ru>
2022-05-02 17:43:32 +0600

    add Task003

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,15 +1,28 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
 Console.Write("Введите число ");
+/*
 string number = Console.ReadLine();
 /*
-int count = number.Length;
+int count = number.Length;   // Первый способ
 Console.WriteLine(count);
 */
 
+/*
+string number = Console.ReadLine();
 int result = 0;
-for(int i = 0; i < number.Length; i++)
+for(int i = 0; i < number.Length; i++)  // Второй способ
 {
     result++;
 }
 
 Console.WriteLine("Otvet: " + result);
+*/
+
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while (number != 0)
+{
+    number = number / 10;
+    index++;
+}
+Console.WriteLine(index);
---
+++ Task003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int fact = Convert.ToInt32(Console.ReadLine());
+
+int sum = 1;
+
+for(int i = 1; i <= fact; i++)
+{
+    sum *= i;
+}
+
+Console.WriteLine("Otvet: " + sum);
