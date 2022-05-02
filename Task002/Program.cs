6b90b1153d3083979fb6b6f5f60c5c1e1db4f0b7
Abylai <aba.omar@mail.ru>
2022-05-02 16:36:39 +0600

    add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+Console.Write("Введите число ");
+string number = Console.ReadLine();
+/*
+int count = number.Length;
+Console.WriteLine(count);
+*/
+
+int result = 0;
+for(int i = 0; i < number.Length; i++)
+{
+    result++;
+}
+
+Console.WriteLine("Otvet: " + result);
