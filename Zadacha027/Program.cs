4a372570456431422a50ddf7f3887ab2f2c11488
Abylai <aba.omar@mail.ru>
2022-05-03 13:54:31 +0600

    Ispravleno

--- Zadacha027/Program.cs
+++ Zadacha027/Program.cs
@@ -4,13 +4,6 @@ Console.Write("Введите число ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int sum = 0;
-/*
-for(int i = 0; i <= number; i++)
-{
-    sum = i;
-}
-Console.WriteLine("Otvet: " + sum); */
-
  while (number > 0)
 {
     sum = sum + number % 10;
