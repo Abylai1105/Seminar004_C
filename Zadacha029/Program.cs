28ac1c044e43aec8e537eab1754bd567bf8f65c9
Abylai <aba.omar@mail.ru>
2022-05-04 11:28:52 +0600

    add Zadacha029

---
+++ Zadacha029/Program.cs
@@ -0,0 +1,10 @@
+﻿// int[] numbers;
+// int[] nums = new int[8];
+
+int[] mas = new int[8];
+Random n = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+mas[i] = n.Next(0, 100);
+Console.Write(mas[i] + " ");
+}
