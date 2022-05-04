f9692f8901df893a8ca0ac55d3d6e778e327bf74
Abylai <aba.omar@mail.ru>
2022-05-04 11:33:28 +0600

    ispravlennaya Zadacha029

--- Zadacha029/Program.cs
+++ Zadacha029/Program.cs
@@ -1,10 +1,9 @@
-﻿// int[] numbers;
-// int[] nums = new int[8];
+﻿// Задача 29 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 int[] mas = new int[8];
 Random n = new Random();
 for (int i = 0; i < mas.Length; i++)
 {
-mas[i] = n.Next(0, 100);
-Console.Write(mas[i] + " ");
+    mas[i] = n.Next(0, 100);
+    Console.Write(mas[i] + " ");
 }
